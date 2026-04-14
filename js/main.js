ac999924a9e885b7199f67eb35ff182980f330ca
inteksolar <inteksolar@gmail.com>
2026-04-14 22:36:03 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -63,6 +63,15 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+document.querySelectorAll(".dropdown > a").forEach(item => {
+  item.addEventListener("click", function(e) {
+    if (window.innerWidth <= 768) {
+      e.preventDefault();
+      this.nextElementSibling.classList.toggle("show-dropdown");
+    }
+  });
+});
+
   /* ===================== FAQ SECTION ===================== */
   document.querySelectorAll(".faq-question").forEach(question => {
     question.addEventListener("click", () => {
